1dbccf5eb48fea6d35da334baaf97e368abb5ade
ohcb <dohyunk0609@gmail.com>
2026-07-14 19:02:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+// app.js (메인 허브)
+
+import { initTimer } from './timer.js';
+import { initTabs } from './tabs.js'; // 💡 탭 기능 불러오기
+
+// 앱 시작 시 기능들 작동시키기
+initTimer();
+initTabs();  // 💡 탭 기능 실행하기
 
