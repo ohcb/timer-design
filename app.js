8804ce597ea138249812de9fbcf5e0e1f09e6b6a
ohcb <dohyunk0609@gmail.com>
2026-07-19 00:50:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-// App.js
+// app.js
 
 // 1. 상단에 import 추가
 import { initTabs } from './tabs.js';
 import { initSolvesManager } from './solve-bottom-sheet.js'; // 💡 새로 만든 파일 연결
+import { initTimer } from './timer.js';
 
 // 앱이 실행될 때 호출되는 초기화 구역
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,4 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // 💡 새로 추가한 바텀 시트 인터랙션 초기화
   initSolvesManager();
   
+  initTimer(); // 💡 타이머 기능 작동 시작!
 });
+
