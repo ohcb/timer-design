12f7ce24ff88475ecf5c40c68bfdd11ae43e83eb
ohcb <dohyunk0609@gmail.com>
2026-07-27 10:57:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,23 +3,15 @@
 import { initTabs } from './tabs.js';
 import { initSolvesManager } from './solve-bottom-sheet.js';
 import { initTimer } from './timer.js';
-import { initStats } from './stats.js'; // 💡 Stats 모듈 import 추가
+import { initStats } from './stats.js';
 import { initSessionManager } from './session-manager.js';
+import { initSolves } from './solves.js'; // 💡 추가
 
-// 앱이 실행될 때 호출되는 초기화 구역
 document.addEventListener('DOMContentLoaded', () => {
-  
-  // 기존 내비게이션 탭 초기화
   initTabs();
-  
-  // 바텀 시트 인터랙션 초기화
   initSolvesManager();
-  
-  // 타이머 기능 초기화
   initTimer();
-
-  // 💡 새로 추가한 스탯 기능 초기화
   initStats();
-
   initSessionManager();
+  initSolves(); // 💡 추가
 });
